b2e4eebf3006a08cd3297fdf2d9f492cb7472da5
Pedro Ventura <pedrojventura@gmail.com>
2013-09-16 23:29:46 +0200

    Estructura inicial de archivos del plugin

---
+++ assets/js/cookie-check.js
@@ -0,0 +1,71 @@
+/**
+ * Libreria JavaScript para incluir el mensaje de advertencia del uso de cookies en la web.
+ * Se incluye una cookie cuando el usuario entra en la web y si sigue navegando el valor de ésta cambia y 
+ * el mensaje no vuelve a aparecer.
+ * Al ser sólo una normativa a nivel de España, solo se mostrará esta advertencia y funcionalidad para los usuarios de este país.
+ * Se necesita la librería jQuery para funcionar y el plugin jquery-cookie https://github.com/carhartl/jquery-cookie
+ * Autor: Pedro Ventura http://www.pedroventura.com
+ * Si lo consideras no dudes en aportar mejoras al código.
+*/
+var CookieLegal = {
+
+	// variable que contiene el nombre de la web o blog, que se usará para setear la cookie
+	web: null,
+
+	// dias que durará la cookie. Por defecto 1 año
+	duracionDias: 365,
+
+	// si se quiere que dure mas de 1 año se usará esta variable para multiplicar por los años deseados
+	anios: 1,
+
+	// funcion para comprobar si el usuario es de España
+	checkGeoUsuario: function _checkGeoUsuario(url) {
+		jQuery.post(url, {action:"geo-ip"}, function(geoUsuario) {
+			if (geoUsuario == 'RD') {
+				CookieLegal.checkCookie();
+				CookieLegal.cargaMensaje();
+			}
+		});
+	},
+
+	// funcion que se encarga de crear y setear la cookie.
+	// la duración de la cookie será de 1 año por
+	checkCookie: function _checkCookie() {
+		laCookie = CookieLegal.leerCookie();
+		if (typeof laCookie != "undefined") {
+			CookieLegal.setearCookie(2);
+		} else {
+			CookieLegal.setearCookie(1);
+		}
+	},
+
+	// crea la cookie con el valor asignado
+	setearCookie: function _setearCookie(valor) {
+		jQuery.cookie('cookie_legal_' + CookieLegal.web, valor , { expires: CookieLegal.duracionDias * CookieLegal.anios, path: '/' });
+	},
+
+	// funcion que se encarga de mostrar el mensaje legal
+	// @todo: Posible mejora para sacar el texto y que se pueda configurar desde el dashboard de WP.
+	cargaMensaje: function _cargaMensaje() {
+		laCookie = CookieLegal.leerCookie();
+		if (laCookie != 2) {
+			jQuery('body').prepend('<div id="wrapperMensajeCookie" class="wrapperMensajeCookie"><div class="inner"><div class="textoLegalCookie"><p><strong>Uso de cookies</strong></p><p>Utilizamos cookies propias y de terceros para mejorar la experiencia de navegación, y ofrecer contenidos y publicidad de interés. Al continuar con la navegación entendemos que se acepta nuestra <a href="/politica-de-cookies/" target="_blank">política de cookies</a>.</p><a onclick="jQuery(\'#wrapperMensajeCookie\').hide();" class="cerrarTextoLegalCookie" title="Cerrar"></a></div></div></div>');
+		}
+	},
+
+	// funcion para leer el valor de la cookie
+	leerCookie: function _leerCookie() {
+		laCookie = parseInt(jQuery.cookie('cookie_legal_' + CookieLegal.web));
+		return laCookie;
+	},
+
+	// funcion de inicialización
+	inicio: function _inicio(setup) {
+		this.web = setup.web;
+		laCookie = this.leerCookie();
+		if (laCookie != 2) {
+			//cuando ya existe la cookie no hace falta seguir haciendo esta comprobacion
+			this.checkGeoUsuario(setup.ajaxCallback);
+		}
+	}
+};
---
+++ cookie-ley-espanola.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Plugin Name: Cookie Ley Española
+ * Plugin URI: http://www.pedroventura.com/internet/plugin-en-wordpress-cumplir-ley-espanola
+ * Description: Este plugin aporta toda la funcionalidad para hacer cumplir la ley de cookies en España
+ * Version: 1.0
+ * Author: Pedro Ventura
+ * Author URI: http://www.pedroventura.com/
+ */
+
+// cargamos los archivos js y css
+add_action( 'wp_enqueue_scripts', 'cargar_archivos' );
+// en el footer insertamos el código js para iniciar toda la funcionalidad
+add_action('wp_footer', 'iniciar_app_cookie');
+
+// declaramos la URL del fichero que maneja las llamadas AJAX (wp-admin/admin-ajax.php)
+wp_localize_script( 'my-ajax-request', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+
+// accion para manejar las acciones sobre nuestra funcion
+do_action( 'wp_ajax_nopriv_' . $_REQUEST['action'] );
+do_action( 'wp_ajax_' . $_POST['action'] );
+
+//conectamos el plugin con las acciones ajax que se ejecutarán
+add_action( 'wp_ajax_nopriv_geo-ip', 'geoUsuario' );
+add_action( 'wp_ajax_geo-ip', 'geoUsuario' );
+
+/**
+ * Funcion base para comprobar la ip del usuario y devolver el codigo de pais.
+ * Esta función esta optimizada para funcionar con el plugin W3 Total Cache, con el que podemos cachear consultas
+ * a la API externa de geoip, la cual tiene un límite de 10,000 llamadas a la hora.
+ * 
+*/
+function geoUsuario() {
+ 	 // compruebo si existe el plugin de W3 Total Cache
+	if (file_exists(WP_PLUGIN_DIR . '/w3-total-cache/lib/W3/ObjectCache.php')) {
+		require_once WP_PLUGIN_DIR . '/w3-total-cache/lib/W3/ObjectCache.php';
+		// creo la instancia de la clase para cachear objectos con el plugin de W3 Total cache
+		$cacheObjeto = & W3_ObjectCache::instance();
+		// compruebo si el cacheo de objectos está activo
+		$cacheActivo = $cacheObjeto->_caching;
+		if ($cacheActivo) {
+			$paisIp = $cacheObjeto->get('ip_' . ipToSlug($_SERVER['REMOTE_ADDR']),'cookieLegal');
+			if ( $paisIp === false ) {
+				$paisIp = obtenerGeoInfo();
+				$cacheObjeto->add('ip_' . ipToSlug($_SERVER['REMOTE_ADDR']), $paisIp, 'cookieLegal', 3600);
+			}
+			echo $paisIp;
+			exit;
+		}
+	} else {
+		echo obtenerGeoInfo();
+		exit;
+	}
+}
+
+/**
+ * Function para obtener los datos geográficos de una IP.
+ * Funcion con el servicio externo de freegeoip.
+ * @link http://freegeoip.net/
+ * @todo conectar con varios servicios, otro ejemplo http://api.hostip.info/get_json.php?ip=XX.XX.XX.XX
+*/
+function obtenerGeoInfo() {
+	$urlIpCheck = 'http://freegeoip.net/json/' .  $_SERVER['REMOTE_ADDR'];
+	$contenido = file_get_contents($urlIpCheck);
+	$parsedJson  = json_decode($contenido);
+	if (isset($parsedJson->country_code)) {
+		return $parsedJson->country_code;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Funcion para crear un slug en base a una ip. 
+ * Se usará como base del id cuando se cachea la consulta
+*/
+function ipToSlug($ip) {
+	return str_replace('.', '_', $ip);
+}
+
+/**
+ * Funcion para cargar las librerias js y el css necesario
+*/
+function cargar_archivos() {
+	wp_enqueue_script(
+		'cookie-check',
+		plugins_url( '/assets/js/cookie-check.js' , __FILE__ ),
+		array( 'jquery' ),
+		'1.0.0'
+		);
+	wp_enqueue_script(
+		'jquery.cookie',
+		plugins_url( '/assets/js/jquery.cookie.js' , __FILE__ ),
+		array( 'jquery' ),
+		'1.0.0'
+		);
+	wp_enqueue_style(
+		'ley-cookie', 
+		plugins_url( '/assets/css/ley-cookie.css' , __FILE__ )
+		);
+}
+
+/**
+ * Funcion que inicializa la api de la libreria js que se encarga de comprobar la cookie y mostrar el mensaje
+*/
+function iniciar_app_cookie() {
+	?>
+	<script type="text/javascript">
+	jQuery(document).ready(function() {
+		CookieLegal.inicio({
+			web: "<?php echo str_replace('http://', '',home_url()); ?>", 
+			ajaxCallback: "wp-admin/admin-ajax.php"
+		});
+	});
+	</script>
+	<?php 
+}
